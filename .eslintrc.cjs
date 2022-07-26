80d1a14bd9a24c2298aa70a62398afdc691b1358
Hogan <hongju110@126.com>
2022-07-26 22:14:20 +0800

    prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     },
     "extends": [
         "plugin:vue/vue3-recommended",
+        "plugin:prettier/recommended"
     ],
     "parserOptions": {
         "ecmaVersion": "latest",
