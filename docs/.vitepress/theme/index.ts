5f82d93f212148b21b05ad8b82d9f70340c19b7f
Hogan <hongju110@126.com>
2022-07-29 10:25:00 +0800

    vitepress-theme-demoblock

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,16 @@
 import DefaultTheme from "vitepress/theme";
 import Test from '../../../src/components/Test'
 import HelloWorld from '../../../src/components/HelloWorld.vue'
+import Demo from "vitepress-theme-demoblock/components/Demo.vue";
+import DemoBlock from "vitepress-theme-demoblock/components/DemoBlock.vue";
+import type {App} from 'vue'
 
 export default {
   ...DefaultTheme,
-  enhanceApp({app}) {
+  enhanceApp({app}: {app:App<never>}) {
     app.component('HelloWorld', HelloWorld)
     app.component('Test', Test)
+    app.component('Demo', Demo)
+    app.component('DemoBlock', DemoBlock)
   }
 }
--- docs/components/button/index.md
+++ docs/components/button/index.md
@@ -1,4 +1,12 @@
 # 按钮 -- button
 
+:::demo HelloWorld
+```vue
+<template>
+  <HelloWorld></HelloWorld>
+</template>
+```
+:::
+
 <HelloWorld></HelloWorld>
 <Test></Test>
